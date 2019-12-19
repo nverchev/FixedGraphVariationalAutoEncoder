705b91599dbf97a5fd8f8eb3de32c06a0ccc4dfe
nicolas <nicolas.vercheval@gmail.com>
2019-12-19 13:34:31 +0100

    debugging

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ for path in sorted(glob.glob("../data_vo/preproc_data/*")):
         if not skip:
             data=np.hstack([data,load_sparse(sample)])
             path_list.append(path)
-ErrorFile=len(data)
+ErrorFile=[len(data)]
 ErrorFile.append({tensor: data[-1][tensor].shape for tensor in data[-1].keys()})
 ErrorFile.append(path_list)
 
@@ -111,7 +111,7 @@ for path in sorted(glob.glob("../scratch_kyukon_vo/preproc_data/*")):
         if not skip:
             data=np.hstack([data,load_sparse(sample)])
             path_list.append(path)
-ErrorFile=len(data)
+ErrorFile=[len(data)]
 ErrorFile.append({tensor: data[-1][tensor].shape for tensor in data[-1].keys()})
 ErrorFile.append(path_list)
 with open("errors.txt", 'w') as file:
