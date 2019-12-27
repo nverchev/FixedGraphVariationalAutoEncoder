dc09eb74cfe3c76838c245f5d3d0c1db32579ac8
nicolas <nicolas.vercheval@gmail.com>
2019-12-27 20:58:57 +0100

    create mean

---
+++ temp.py
@@ -0,0 +1,23 @@
+from minio import Minio
+from minio.error import ResponseError
+import glob
+import tarfile
+import shutil
+minioClient = Minio('s3nverchev.ugent.be',
+                  access_key='M12AX6PRAW2HUPUHPTL0',
+                  secret_key='yC2UlxBD+exlGz5S+zLaVclYUvRQ9D8msgOMVAWh',secure=True)
+
+
+
+for subject in sorted(glob.glob("../data_vo/*")):
+    for operator in sorted(glob.glob(subject)):
+        tarname=operator.split('/')[-1]+'_'+str(subject[-2:-1])+".tar.gz"
+        tar = tarfile.open(tarname, "w:gz")
+        tar.add(operator)
+        tar.close()
+        minioClient.fput_object('coma', tarname, tarname)
+        shutil.rmtree(tarname)
+
+
+
+
